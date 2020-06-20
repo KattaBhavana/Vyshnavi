892c44cc2edc28c3dc66db7ab8f8887621cd04dc
GanatheVyshnavi <67085766+GanatheVyshnavi@users.noreply.github.com>
2020-06-20 11:45:26 +0530

    Add files via upload

---
+++ 9th.py
@@ -0,0 +1,7 @@
+a=int(input("enter value of a "))
+b=int(input("enter value of b"))
+print("value of a and b before swapping is",a,b)
+temp=a
+a=b
+b=temp
+print("value of a and b after swapping is", a,b)

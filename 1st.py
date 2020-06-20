759d3a260fb4d7ad0e879273c1774a6131d4772e
GanatheVyshnavi <67085766+GanatheVyshnavi@users.noreply.github.com>
2020-06-20 08:27:39 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,36 @@
+x =int(input("x is"))
+y=int(input("y is"))
+z=x+y
+print('addittion of x and y is',z)
+
+
+x=float(input('x is'))
+y=float(input('y is'))
+z=x-y
+print('subtractiin of x and y is:',z)
+
+x=int(input("x is"))
+y=float(input('y is'))
+z=x*y
+print('multiplication of x and y',z)
+
+x=int(input("x is"))
+y=int(input("y is"))
+z=x/y
+print('division of x and y',int(z))
+
+x=float(input('x is'))
+y=float(input("y is"))
+z=x%y
+print('modulus is',z)
+
+x=float(input('x is'))
+y=float(input('y is'))
+z=x**y
+print('exponent is',z)
+
+
+x=float(input('x is'))
+y=float(input('y is'))
+z=x//y
+print('floor division is',z)
